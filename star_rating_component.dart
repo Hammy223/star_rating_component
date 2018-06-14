0b8b428331fb4c842a4868d3d837eb8aed87562d
Byron <bypalm@gmail.com>
2018-06-14 15:43:40 -0600

    working

--- star_rating_component.dart
+++ star_rating_component.dart
@@ -1,12 +1,16 @@
-import 'package:angular/angular.dart';
 import 'dart:async';
+
+import 'package:angular/angular.dart';
 import 'package:angular_components/material_button/material_button.dart';
 import 'package:angular_components/material_icon/material_icon.dart';
 
 @Component(
   selector: 'mat-star-rating',
   templateUrl: './star_rating_component.html',
-  styleUrls: ['./star_rating_component.css'],
+  styleUrls: [
+    './star_rating_component.css',
+    "https://fonts.googleapis.com/icon?family=Material+Icons"
+  ],
   encapsulation: ViewEncapsulation.Emulated,
   directives: [
     MaterialButtonComponent,
@@ -18,39 +22,30 @@ class StarRatingComponent implements OnInit {
 
   @Input() int rating;
   @Input() int starCount;
-  @Input() String color;
+  @Input() String starColor;
   final ratingUpdated = new StreamController.broadcast();
-  @Output() Stream get onRatingUpdated => ratingUpdated.stream;
+  @Output() Stream get onRatingClick => ratingUpdated.stream;
 
-  int snackBarDuration = 2000;
-  var ratingArr;
-
-
+  var ratingArr = [];
 
   @override
   void ngOnInit() {
-    for (var index = 0; index < this.starCount; index++) {
-      this.ratingArr.add(index);
+    for (var index = 0; index < starCount; index++) {
+      ratingArr.add(index);
     }
   }
 
   onClick(int rating) {
-    print(rating);
+    this.rating = rating;
     ratingUpdated.add(rating);
     return false;
   }
 
   showIcon(int index) {
-    if (this.rating >= index + 1) {
+    if (rating >= index + 1) {
       return 'star';
     } else {
       return 'star_border';
     }
   }
-}
-
-enum StarRatingColor {
-  primary,
-  accent,
-  warn
 }
